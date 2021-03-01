490fa98d92a1e528fda0805035e82ef36de28086
ujjwal kumar <79785180+ujjwal002@users.noreply.github.com>
2021-03-01 17:08:36 +0530

    Update Left rotation of array with O(n) and O(1).py

--- day-1/array/Left rotation of array with O(n) and O(1).py
+++ day-1/array/Left rotation of array with O(n) and O(1).py
@@ -8,21 +8,16 @@ def leftRotate(arr, n, k):
     print
     return
  
- 
-
 arr = [1, 3, 5, 7, 9]
 n = len(arr)
 k = 2
  
-
 leftRotate(arr, n, k)
  
 k = 3
  
-
 leftRotate(arr, n, k)
  
 k = 4
  
-
 leftRotate(arr, n, k)
