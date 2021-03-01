fc18fd41f77be12a5a4b084a545a56e219f0242b
ujjwal kumar <79785180+ujjwal002@users.noreply.github.com>
2021-03-01 16:30:15 +0530

    Update array_rotation.py

--- day-1/array/array_rotation.py
+++ day-1/array/array_rotation.py
@@ -1,12 +1,12 @@
-array = [1,2,3,4,5]
-shift = 2
+array = [1,2,3,4,5] #declare array
+shift = 2 #number of rotation want to perform
 
 for i in range (0,shift):
-  temp = array[0]
+  temp = array[0] #save first element of array in temp variable
   
   for j in range (0, len(array)-1):
-    array[j]= array[j+1]
-  array[j] = temp
+    array[j]= array[j+1] #shift element toward left  side
+  array[j] = temp #copy temp value to last elements array
   
   for i in range (0,len(array)):
     print(array[i])
