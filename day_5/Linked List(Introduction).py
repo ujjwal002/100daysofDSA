688871b0a8f678d22f1f3f794693f9ab91530324
ujjwal kumar <79785180+ujjwal002@users.noreply.github.com>
2021-03-08 08:08:19 +0530

    Create Linked List(Introduction).py

---
+++ day_5/Linked List(Introduction).py
@@ -0,0 +1,11 @@
+class Node: 
+   
+    def __init__(self, data): 
+        self.data = data  # Assign data 
+        self.next = None  # Initialize  
+                          
+
+class LinkedList: 
+     
+    def __init__(self):   # List object 
+        self.head = None
